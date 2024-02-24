4d9c7621af66d7e3feae4b060ffe7e4bf3856d94
Ronny Wang <ronnywang@gmail.com>
2024-02-24 14:35:45 +0800

    抓取縣市、鄉鎮、村里列表

---
+++ get-village-data.php
@@ -0,0 +1,7 @@
+<?php
+
+// https://www.ris.gov.tw/documents/html/5/1/167.html
+// https://www.ris.gov.tw/documents/html/5/1/168.html
+system("wget -O county.txt https://www.ris.gov.tw/documents/data/5/1/RSCD0102.txt");
+system("wget -O town.txt https://www.ris.gov.tw/documents/data/5/1/RSCD0103.txt");
+system("wget -O village.txt https://www.ris.gov.tw/documents/data/5/1/11109VillageUTF8.txt");
