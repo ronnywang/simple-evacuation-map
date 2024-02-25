4be606ec541244ee1f5884ec1a52899e807815c0
Ronny Wang <ronnywang@gmail.com>
2024-02-25 16:56:34 +0800

    add Helper

---
+++ Helper.php
@@ -0,0 +1,79 @@
+<?php
+
+class Helper
+{
+    protected static $_towns = null;
+    protected static $_villages = null;
+
+    public static function http($url)
+    {
+        error_log($url);
+        $cache_file = __DIR__ . '/cache/' . str_replace('/', '_', $url);
+        if (file_exists($cache_file)) {
+            return file_get_contents($cache_file);
+        }
+        $ch = curl_init($url);
+        // useragent chrome
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $html = curl_exec($ch);
+        curl_close($ch);
+        file_put_contents($cache_file, $html);
+        return $html;
+    }
+
+    protected static function getTowns()
+    {
+        if (!is_null(self::$_towns)) {
+            return;
+        }
+        $fp = fopen(__DIR__ . '/village.txt', 'r');
+        self::$_villages = [];
+        while ($values = fgetcsv($fp)) {
+            list($village_id, $town_id, $name) = $values;
+            self::$_villages[$village_id] = $name;
+        }
+        $fp = fopen(__DIR__ . '/town.txt', 'r');
+        $map = [];
+        while ($line = fgets($fp)) {
+            $line = trim($line);
+            list($id, $name) = explode('=', $line, 2);
+            if (substr($id, -3) != '000') {
+                $parent_id = sprintf("%05d000", substr($id, 0, 5));
+                if (isset($map[$parent_id])) {
+                    $name = str_replace($map[$parent_id], '', $name);
+                }
+            }
+            $map[$id] = $name;
+        }
+        self::$_towns = $map;
+    }
+
+    public static function getTownId($county_id, $town_name)
+    {
+        self::getTowns();
+        foreach (self::$_towns as $id => $name) {
+            if (substr($id, 0 , 5) != $county_id) {
+                continue;
+            }
+            if ($name === $town_name) {
+                return $id;
+            }
+        }
+        throw new Exception("Town not found: $county_id, $town_name");
+    }
+
+    public static function getVillageId($town_id, $village_name)
+    {
+        self::getTowns();
+        foreach (self::$_villages as $id => $name) {
+            if (strpos($id, $town_id) !== 0) {
+                continue;
+            }
+            if ($name === $village_name) {
+                return $id;
+            }
+        }
+        throw new Exception("Village not found: $town_id, $village_name");
+    }
+}
